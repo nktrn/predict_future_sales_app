c3e8f68159201cd49b809b4fd338f15c4345d548
Nikita Aparovich <o.pelmen.o@gmail.com>
2022-11-09 19:23:49 +0300

    ...

--- pfs/catboos_model.py
+++ pfs/catboos_model.py
@@ -12,21 +12,17 @@ class SalesPrediction:
     
     def predict(self, shop_id):
         data = [34, int(shop_id['shop_id']), 30, 10, 40, 'Балашиха', 'shop. center']
-        predict = self.model.predict(data)
+        predict = 0#self.model.predict(data)
         f = ['date_block_num', 'shop_id', 'item_id', 'lag', 'item_category_id', 'shop_city', 'shop_location']
         label = ['label']
         df = pd.DataFrame(
             columns=f + label
         )
         data += [predict]
-
         df.loc[len(df)] = data
 
         explainer = shap.TreeExplainer(self.model)
-
         shap_values = explainer(df[f])
-
-
         buf = BytesIO()
         #shap.force_plot(explainer.expected_value, shap_values, df[f], matplotlib = True, show = False)
         shap.waterfall_plot(shap_values[0], show = False)
